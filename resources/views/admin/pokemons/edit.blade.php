0c8aa130efa4c30e13c77347d1c20e2797964eaf
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-09 17:41:44 +0100

    added values to Form in edit.blade

--- resources/views/admin/pokemons/edit.blade.php
+++ resources/views/admin/pokemons/edit.blade.php
@@ -14,62 +14,62 @@
                         <label for="titolo"class="form-label">
                             Edit pokemon's name
                         </label>
-                        <input type="text" name="name" id="name" class="form-control">
+                        <input type="text" value="{{$pokemon->name}}" name="name" id="name" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="img_url"class="form-label">Edit pokemon picture</label>
-                        <input type="text" name="img_url" id="img_url" class="form-control">
+                        <input type="text" value="{{$pokemon->img_url}}" name="img_url" id="img_url" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="species"class="form-label">Edit pokemon species:</label>
-                        <input type="text" name="species" id="species" class="form-control">
+                        <input type="text" value="{{$pokemon->species}}" name="species" id="species" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="height"class="form-label">edit height:</label>
-                        <input type="text" name="height" id="height" class="form-control">
+                        <input type="text" value="{{$pokemon->height}}" name="height" id="height" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="series"class="form-label">edit weight</label>
-                        <input type="weight" name="weight" id="weight" class="form-control">
+                        <input type="weight" value="{{$pokemon->weight}}" name="weight" id="weight" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="abilities"class="form-label">edit abilities</label>
-                        <input type="abilities" name="abilities" id="abilities" class="form-control">
+                        <input type="abilities" value="{{$pokemon->abilities}}" name="abilities" id="abilities" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="hp"class="form-label">edit hp</label>
-                        <input type="hp" name="hp" id="hp" class="form-control">
+                        <input type="hp"  value="{{$pokemon->hp}}" name="hp" id="hp" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="attack"class="form-label">edit attack</label>
-                        <input type="attack" name="attack" id="attack" class="form-control">
+                        <input type="attack"  value="{{$pokemon->attack}}" name="attack" id="attack" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="defense"class="form-label">edit defense</label>
-                        <input type="defense" name="defense" id="defense" class="form-control">
+                        <input type="defense" value="{{$pokemon->defense}}" name="defense" id="defense" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="speed_attack"class="form-label">edit speed_attack</label>
-                        <input type="speed_attack" name="speed_attack" id="speed_attack" class="form-control">
+                        <input type="speed_attack" value="{{$pokemon->speed_attack}}" name="speed_attack" id="speed_attack" class="form-control">
                     </div>
 
                     <div class="mb-3">
-                        <label for="speed_defence"class="form-label">edit speed_attack</label>
-                        <input type="speed_defence" name="speed_defence" id="speed_defence" class="form-control">
+                        <label for="speed_defence"class="form-label">edit speed_defence</label>
+                        <input type="speed_defence" value="{{$pokemon->speed_defence}}" name="speed_defence" id="speed_defence" class="form-control">
                     </div>
 
                     <div class="mb-3">
                         <label for="speed"class="form-label">edit speed</label>
-                        <input type="speed" name="speed" id="speed" class="form-control">
+                        <input type="speed" value="{{$pokemon->speed}}" name="speed" id="speed" class="form-control">
                     </div>
 
                     
